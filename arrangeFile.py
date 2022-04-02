bcb281e4bfc774567ac74a3f7e6481d15d53dc93
HypertextAssassin <72544486+HypertextAssassin69@users.noreply.github.com>
2022-04-02 18:46:18 +0530

    Add files via upload

---
+++ arrangeFile.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+directory = input("please enter the path of the folder which you want top clear: ")
+listOfItem = os.listdir(directory)
+
+print(listOfItem)
+for item in listOfItem:# each file or folder from listOfItem will be used
+    print(item)
+    noFileType = item.split('.')
+    print(noFileType)
+    print(len(noFileType))
+    if len(noFileType) >1:
+        splitext ,e = os.path.splitext(directory+"\\"+item)
+        print (splitext+" + "+e)
+        print(e.split('.'))
+        dot,fileExtension = e.split('.')
+        print(fileExtension)
+        if os.path.exists(directory+"\\"+fileExtension+" folder") == True:
+            shutil.move(directory+"\\"+item,directory+"\\"+fileExtension+" folder")
+        else :
+            fileForWanderers =os.makedirs(directory+"\\"+fileExtension+" folder")
+            shutil.move(directory+"\\"+item,directory+"\\"+fileExtension+" folder")
